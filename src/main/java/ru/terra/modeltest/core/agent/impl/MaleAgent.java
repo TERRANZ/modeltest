80809a02648cc1441265dbd9c8a7a00ba484ce96
vadim.korostelev <Vadim.Korostelev@thomsonreuters.com>
2017-09-01 13:05:41 +0300

    incr

--- src/main/java/ru/terra/modeltest/core/agent/impl/MaleAgent.java
+++ src/main/java/ru/terra/modeltest/core/agent/impl/MaleAgent.java
@@ -1,10 +1,7 @@
 package ru.terra.modeltest.core.agent.impl;
 
 import ru.terra.modeltest.core.agent.Agent;
-import ru.terra.modeltest.core.handler.impl.CheckFriendMessageHandler;
-import ru.terra.modeltest.core.handler.impl.FemaleToMaleFriendsMessageHandler;
-import ru.terra.modeltest.core.handler.impl.FriendshipAcceptedMessageHandler;
-import ru.terra.modeltest.core.handler.impl.MaleToMaleFriendsMessageHandler;
+import ru.terra.modeltest.core.handler.impl.*;
 import ru.terra.modeltest.core.message.impl.*;
 
 public class MaleAgent extends Agent {
@@ -15,7 +12,7 @@ public class MaleAgent extends Agent {
         addHandler(FriendCheckedMessage.class, new FemaleToMaleFriendsMessageHandler());
         addHandler(CheckFriendMessage.class, new CheckFriendMessageHandler());
         addHandler(FriendshipAcceptedMessage.class, new FriendshipAcceptedMessageHandler());
-        addHandler(FemaleFriendsAcceptMessage.class, new FemaleToMaleFriendsMessageHandler());
+        addHandler(FemaleFriendsAcceptMessage.class, new FemaleFriendsMessageHandler());
     }
 
 }
